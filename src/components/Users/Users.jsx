86ddef383654c71e88b157a3c5c54ea2e1305edc
Al Hasanul Banna <tutul5800@gmail.com>
2023-05-12 22:28:45 +0600

    deleted data from ui

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
-  const users = useLoaderData();
+  const loadedUser = useLoaderData();
+  const [users, setUsers] = useState(loadedUser);
 
   const handleDelete = (_id) => {
     console.log(_id);
@@ -14,6 +15,8 @@ const Users = () => {
         console.log(data);
         if (data.deletedCount > 0) {
           alert("Deleted successfully");
+          const remaining = users.filter((user) => user._id !== _id);
+          setUsers(remaining);
         }
       });
   };
